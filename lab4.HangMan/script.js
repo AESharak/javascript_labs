31b110486719755edfffb2ab43845b2f1640a8ee
Ahmed Essam <ahmed.essam.sharak@gmail.com>
2025-01-11 08:10:58 +0200

    finishing hangman game

---
+++ lab4.HangMan/script.js
@@ -0,0 +1,110 @@
+// Main DOM ELEMENTS
+var keyboardSection = document.getElementById("keyboard");
+var wordDisplayDiv = document.getElementById("word-area");
+var hintPart = document.getElementById("hintContent");
+var guessesCountDisplay = document.getElementById("wrongGuesses");
+var hangManImage = document.querySelector(".hangman-img img");
+var gameModalSection = document.getElementById("finishedModal");
+var playAgainButton = document.getElementById("playAgain");
+
+// Global Variables
+var currentWord = "",
+  correctLetters,
+  wrongGuesses,
+  maxWrongGuesses = 6;
+
+function resetGame() {
+  correctLetters = [];
+  wrongGuesses = 0;
+  wordDisplayDiv.innerHTML = currentWord
+    .split("")
+    .map(() => `<li class='letter'></li>`)
+    .join("");
+  keyboardSection
+    .querySelectorAll("button")
+    .forEach((key) => (key.disabled = false));
+  guessesCountDisplay.textContent = `${wrongGuesses} / ${maxWrongGuesses}`;
+  hangManImage.src = `./assets/images/hangman-${wrongGuesses}.svg`;
+  gameModalSection.classList.remove("show");
+}
+
+function generateRandomWord() {
+  var categories = [
+    "animals",
+    "fruits",
+    "celebrities",
+    "countries",
+    "countries",
+  ];
+  resetGame();
+  var randomCategory =
+    categories[Math.floor(Math.random() * categories.length)];
+  var currentWordData =
+    wordData[randomCategory][
+      Math.floor(Math.random() * wordData[randomCategory].length)
+    ];
+
+  currentWord = currentWordData.word;
+
+  console.log(
+    currentWordData.word,
+    currentWordData.hint,
+    currentWordData.level
+  );
+  hintPart.textContent = currentWordData.hint;
+  wordDisplayDiv.innerHTML = currentWordData.word
+    .split("")
+    .map((letter) => `<li class='letter'></li>`)
+    .join("");
+}
+
+function gameOver(finishedStatus) {
+  setTimeout(() => {
+    var modalText = finishedStatus ? "You found the word!" : "Game Over!";
+    gameModalSection.querySelector("img").src = `./assets/images/${
+      finishedStatus ? "victory" : "lost"
+    }.gif`;
+    gameModalSection.querySelector("h3").textContent = `${
+      finishedStatus ? "Congratulations!" : "Game Over!"
+    }`;
+    gameModalSection.querySelector(
+      "p"
+    ).innerHTML = `${modalText} <span>${currentWord}</span>`;
+    gameModalSection.classList.add("show");
+  }, 250);
+}
+
+function clickAKey(key, letterClicked) {
+  if (currentWord.includes(letterClicked)) {
+    currentWord.split("").forEach((letter, index) => {
+      if (letter === letterClicked) {
+        correctLetters.push(letter);
+        wordDisplayDiv.querySelectorAll("li")[index].textContent = letter;
+        wordDisplayDiv.querySelectorAll("li")[index].classList.add("guessed");
+      }
+    });
+  } else {
+    wrongGuesses++;
+    hangManImage.src = `./assets/images/hangman-${wrongGuesses}.svg`;
+  }
+  guessesCountDisplay.textContent = `${wrongGuesses} / ${maxWrongGuesses}`;
+  key.disabled = true;
+
+  if (wrongGuesses === maxWrongGuesses) return gameOver(false);
+  if (correctLetters.length === currentWord.length) return gameOver(true);
+}
+
+generateRandomWord();
+
+for (var j = 97; j <= 122; j++) {
+  var key = document.createElement("button");
+  key.innerText = String.fromCharCode(j);
+  keyboardSection.appendChild(key);
+  (function (currentJ) {
+    key.addEventListener("click", (e) =>
+      clickAKey(e.target, String.fromCharCode(currentJ))
+    );
+  })(j);
+}
+
+playAgainButton.addEventListener("click", generateRandomWord);
---
+++ lab4.HangMan/words-data.js
@@ -0,0 +1,69 @@
+const wordData = {
+  animals: [
+    { word: "cat", hint: "Common household pet", level: "easy" },
+    {
+      word: "elephant",
+      hint: "Largest land mammal with a trunk",
+      level: "medium",
+    },
+    {
+      word: "hippopotamus",
+      hint: "Large semi-aquatic African mammal",
+      level: "hard",
+    },
+    { word: "dog", hint: "Man's best friend", level: "easy" },
+    { word: "rhinoceros", hint: "Horned African mammal", level: "hard" },
+  ],
+  fruits: [
+    { word: "apple", hint: "Red or green common fruit", level: "easy" },
+    {
+      word: "dragonfruit",
+      hint: "Pink/white exotic fruit with black seeds",
+      level: "hard",
+    },
+    {
+      word: "kiwi",
+      hint: "Brown fuzzy fruit with green flesh",
+      level: "medium",
+    },
+    { word: "mango", hint: "Sweet tropical fruit", level: "easy" },
+    {
+      word: "pomegranate",
+      hint: "Red fruit filled with seeds",
+      level: "medium",
+    },
+  ],
+  celebrities: [
+    { word: "madonna", hint: "Queen of Pop", level: "easy" },
+    {
+      word: "schwarzenegger",
+      hint: "Famous action movie star and politician",
+      level: "hard",
+    },
+    { word: "dicaprio", hint: "Titanic actor", level: "medium" },
+    { word: "beyonce", hint: "Former Destiny's Child member", level: "easy" },
+    { word: "spielberg", hint: "Famous movie director", level: "medium" },
+  ],
+  countries: [
+    { word: "usa", hint: "North American superpower", level: "easy" },
+    { word: "madagascar", hint: "Large African island nation", level: "hard" },
+    { word: "italy", hint: "Known for pizza and pasta", level: "easy" },
+    { word: "kazakhstan", hint: "Largest landlocked country", level: "hard" },
+    { word: "spain", hint: "Famous for bullfighting", level: "medium" },
+  ],
+  sport: [
+    { word: "golf", hint: "Played with clubs and balls", level: "easy" },
+    { word: "basketball", hint: "Bounce and shoot into hoop", level: "medium" },
+    {
+      word: "synchronized swimming",
+      hint: "Water-based performance sport",
+      level: "hard",
+    },
+    { word: "tennis", hint: "Played with rackets", level: "easy" },
+    {
+      word: "pentathlon",
+      hint: "Five different athletic events",
+      level: "hard",
+    },
+  ],
+};
